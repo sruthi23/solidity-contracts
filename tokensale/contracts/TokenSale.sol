b95ce19f1689ef0b4b220800a947e5b962bdc7b6
sruthi23 <sruthi2525@gmail.com>
2018-02-02 17:35:13 +0530

    Removed 'transferApprove' function

--- tokensale/contracts/TokenSale.sol
+++ tokensale/contracts/TokenSale.sol
@@ -11,56 +11,52 @@ contract TokenSale {
 	uint256 public token;
 	address public admin;
 
+	event EventAdmin(address ad);
+
 	function TokenSale() public {
 
-		demotoken = new ChainToken();
+		demotoken = createToken();
 		admin = msg.sender;
 	}  
 
-/* function createToken() internal returns(ChainToken) {
+	function createToken() internal returns(ChainToken) {
 
- 	return new ChainToken();
- 	}*/
+		return new ChainToken();
+	}
 
- 	modifier onlyAdmin {
- 		
- 		require(msg.sender == admin);
- 		_;
- 	}
+	modifier onlyAdmin {
+		
+		require(msg.sender == admin);
+		_;
+	}
 
- 	function setList(address _adr, uint256 _token) onlyAdmin public {
- 		
- 		addresslist[_adr] = _token;
- 		addresskey.push(_adr);
- 		
- 	}
+	function setList(address _adr, uint256 _token) onlyAdmin public {
+		
+		addresslist[_adr] = _token;
+		addresskey.push(_adr);
+		
+	}
 
- 	function tokenTransfer(address _adr) onlyAdmin public {
- 		
- 		token = addresslist[_adr];
- 		require(token != 0);
- 		demotoken.transfer(_adr,token);
- 		addresslist[_adr] = 0;
- 	}
+	function tokenTransfer (address _adr) onlyAdmin public {
+		
+		token = addresslist[_adr];
+		require(token != 0);
+		demotoken.transfer(_adr,token);
+		addresslist[_adr] = 0;
+	}
 
- 	function transferApprove(address spender,uint256 _token)public returns(bool){
- 		
- 		demotoken.approve(spender,_token);
- 	}
+	function tokenTransferFrom(address from, address to, uint256 _token) onlyAdmin public {
+		
+		demotoken.transferFrom(from,to,_token);
+	}
 
- 	function tokenTransferFrom(address from, address to, uint256 _token) onlyAdmin public {
- 		
- 		demotoken.transferFrom(from,to,_token);
- 	}
+	function getList() public view returns(address[]){
+		return addresskey;
+	}
+	
+	function balance(address _owner) public view returns(uint256) {
+		
+		return demotoken.balanceOf(_owner);
+	} 
 
- 	function getList() public view returns(address[]){
- 		return addresskey;
- 	}
-
- 	function balance(address _owner,address _spender) public view returns(uint256) {
- 		
- 		return demotoken.allowance(_owner,_spender);
- 	}
-
-
- }
+}
