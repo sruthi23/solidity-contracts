63b2ec4545ee94bad5e6045683fa0f8ad752a081
sruthi23 <sruthi2525@gmail.com>
2018-02-07 17:37:39 +0530

    Modified migration file

--- tokensale/migrations/2_tokensale.js
+++ tokensale/migrations/2_tokensale.js
@@ -1,9 +1,23 @@
 var Tokensale = artifacts.require("./TokenSale.sol");
+var Chaintoken = artifacts.require("./ChainToken.sol");
 
-module.exports = function(deployer, network, accounts) {
+/*module.exports = async function(deployer, network, accounts) {
+
 	deployer.deploy(Tokensale).then(async () => {
 		const instance = await Tokensale.deployed();
+		console.log(instance);
 		const token = await instance.token.call();
+		console.log(token);
 		console.log('Token Address', token);
 	});
+
+
+};
+*/
+module.exports = async function(deployer) {
+	deployer.deploy(Chaintoken).then(async () => {
+		const instance = await Chaintoken.deployed();
+		console.log(instance.address);
+		return deployer.deploy(Tokensale,instance.address);
+	})
 };
