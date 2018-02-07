68c2d7d32c2e3e07059bf0fb0185a8c4262c45dc
sruthi23 <sruthi2525@gmail.com>
2018-02-07 17:50:39 +0530

    Modified constructor

--- tokensale/contracts/TokenSale.sol
+++ tokensale/contracts/TokenSale.sol
@@ -6,61 +6,74 @@ import './ChainToken.sol';
 contract TokenSale {
 
 	ChainToken public demotoken;
-	mapping(address => uint256) addresslist;
+	mapping (address => uint256) addresslist;
 	address[] addresskey;
-	uint256 public token;
+	uint256 public token = 0;
 	address public admin;
 	uint256 public tokencount;
 	uint8 public count;
 
 	event EventAdmin(address ad);
 
-	function TokenSale() public {
+	function TokenSale(address _tokenaddress) public {
 
-		demotoken = createToken();
+		demotoken = createToken(_tokenaddress); //ChainToken(_tokenaddress); //createToken();
 		admin = msg.sender;
 	}  
 
-	function createToken() internal returns(ChainToken) {
+	function createToken(address _tokenaddress) internal pure returns(ChainToken) {
 
-		return new ChainToken();
+		return ChainToken(_tokenaddress);
+	} 
+	
+	function getAdmin() public view returns(address){
+
+		return admin;
 	}
 
 	modifier onlyAdmin {
-		
+
 		require(msg.sender == admin);
 		_;
 	}
 
-	function setList(address _adr, uint256 _token) onlyAdmin public {
+	function setList(address _adr, uint256 _token) onlyAdmin public returns (bool) {
 		require(tokencount <= demotoken.getSupply());
 		addresslist[_adr] = _token;
 		addresskey.push(_adr);
 		tokencount += _token;
+		return true;
 	}
 
 	function tokenTransfer (address _adr) onlyAdmin public {
-		
+
 		require(count <= 2000);
 		token = addresslist[_adr];
 		require(token != 0);
 		demotoken.transfer(_adr,token);
 		addresslist[_adr] = 0;
 		count += 1;
+
 	}
 
 	function tokenTransferFrom(address from, address to, uint256 _token) onlyAdmin public {
-		
+
 		demotoken.transferFrom(from,to,_token);
 	}
 
 	function getList() public view returns(address[]){
+
 		return addresskey;
 	}
-	
+
 	function balance(address _owner) public view returns(uint256) {
-		
+
 		return demotoken.balanceOf(_owner);
 	} 
+
+	function allowance(address _owner,address _spender) public view returns(uint256) {
+
+		return demotoken.allowance(_owner,_spender);
+	}
 
 }
