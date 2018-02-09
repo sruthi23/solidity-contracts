b3b26e0e4ce90e71ca453cc5679af328b9d6f2db
sruthi23 <sruthi2525@gmail.com>
2018-02-09 17:27:54 +0530

    Modified to check require condition

--- tokensale/test/Test.js
+++ tokensale/test/Test.js
@@ -1,33 +1,41 @@
 var TokenSale = artifacts.require("TokenSale");
 
-contract('TokenSale', function() {
+contract('TokenSale', function(accounts) {
 
-	it("should assert true", function() {
-		var tc = TokenSale.deployed();
+	it("should assert true", async function() {
+		var tc = await TokenSale.deployed();
 		assert.isTrue(true);
 		console.log('token created',tc)
 
 	});
 	
-	it("setting list",async function(){
+	it("allow to set list only by admin",async function(){
 
 		var tc = await TokenSale.deployed();
 		//console.log("r u there?");
-		var b = await tc.setList('0xf17f52151ebef6c7334fad080c5704d77216b732',1,{from :'0x627306090abab3a6e1400e9345bc60c78a8bef57'});
+		var b = await tc.setList('0xf17f52151ebef6c7334fad080c5704d77216b732',1);
 		assert.notEqual(b,true,"failed to add in list");
 		
 	});
 
-	it("token transfer",async function(){
+	it("Reject non-admin from setting list",async function(){
+
+		var b =await TokenSale.deployed().then(function(i){return i.setList(accounts[2],1,{from :accounts[1]})});
+		assert.notEqual(b,true,"failed to add list by non-admin");
+	});
+
+	it("token transfer by admin",async function(){
 
 		var tc = await TokenSale.deployed();
 		//console.log("r u there?");
 		//var b = await tc.setList('0xf17f52151ebef6c7334fad080c5704d77216b732',1,{from :'0x627306090abab3a6e1400e9345bc60c78a8bef57'});
-		var c = tc.tokenTransfer('0xf17f52151ebef6c7334fad080c5704d77216b732',{from :'0x627306090abab3a6e1400e9345bc60c78a8bef57'});//tc.tokenTransfer('0xf17f52151ebef6c7334fad080c5704d77216b732');
+		var c = await tc.tokenTransfer('0xf17f52151ebef6c7334fad080c5704d77216b732',{from :'0x627306090abab3a6e1400e9345bc60c78a8bef57'});//tc.tokenTransfer('0xf17f52151ebef6c7334fad080c5704d77216b732');
 		console.log("here!!!!!!!!",c);
 		assert(c != true,"failed");
-		
+
 	});
+
+	it()
 
 
 });
--- tokensale/truffle.js
+++ tokensale/truffle.js
@@ -1,9 +1,32 @@
 module.exports = {
 	networks: {
 		development: {
-			host: "localhost",
+			host: 'localhost',
 			port: 9545,
-      network_id: "*" // Match any network id
-  }
+			//gas: 4700000,
+			network_id: "*"
+		}
+	}
 }
-};
+
+/*ChainToken: 0xe7c0ea356f781e9782ee163fa730e3f8fd6266e7
+0xe7c0ea356f781e9782ee163fa730e3f8fd6266e7
+  Deploying TokenSale...
+  ... 0xb3c0405860dd1a6ad65e9bf76828da16688229c4627afa908891001aec9143c0
+  TokenSale: 0x9dd83f696c358a545c8cd8c9b057af0cdcda6853
+  */
+/*Available Accounts
+==================
+(0) 0xae757d24edb8734a16f11329b6ceb0ff827220f9
+(1) 0x41859aa3e8b7a7def270483b8859654aaff861ff
+(2) 0xcbb177ac930a6475195bf9c5e6f01b948192121e
+(3) 0x7345b6870ae377c4394d8440662f5656df5af9cf
+(4) 0xdf1880b4a141a1e0e378baa3d780aba84cf75cf0
+(5) 0x4d90ff4838d1cbd0b4f25d455ae03cc469855d6b
+(6) 0x1a8bee0b68476c133e27be7acca2287e7ab3734c
+(7) 0xa8e308143d02e68608166932ef3370db364a9933
+(8) 0x5545b2e14d8d028ea9c34f0c2ac91ea6237e00ff
+(9) 0xc1b25d81d1b4491bcd2392fe84d637f72dc88c8d
+TokenSale.deployed().then(function(i){return i.tokenTransfer("0xc09bcaeeecdb9cba49c5441904827f5c88921a8a")})
+
+*/
