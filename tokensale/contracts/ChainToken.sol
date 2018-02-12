00dc832845cc154d5afba7955ccabcac2a4e4590
sruthi23 <sruthi2525@gmail.com>
2018-02-12 17:37:17 +0530

    Overridden transferFrom function

--- tokensale/contracts/ChainToken.sol
+++ tokensale/contracts/ChainToken.sol
@@ -9,10 +9,29 @@ contract ChainToken is StandardToken {
 	uint8 public constant decimals = 18;
 	uint256 public constant total_supply = 1000000  * (1 ether / 1 wei);
 	address public owner;
+	address public admin;
 	
 	function ChainToken() public {
 		totalSupply_ = total_supply;
 		owner = msg.sender;
 		balances[owner] = total_supply;
 	}
+
+	function transferFrom(address _from,address _to,uint256 _value,address _admin) public{
+
+		admin = _admin;
+		transferFrom(_from, _to, _value);
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+		require(_to != address(0));
+		require(_value <= balances[_from]);
+		require(_value <= allowed[_from][admin]);
+		balances[_from] = balances[_from].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		allowed[_from][admin] = allowed[_from][admin].sub(_value);
+		Transfer(_from, _to, _value);
+		return true;
+	}
+
 }
